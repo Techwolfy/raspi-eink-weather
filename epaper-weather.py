b6257986b2f04307fe6c0ea2867ec13408e10153
Techwolf <dring@wolfishly.me>
2022-09-27 02:17:07 -0700

    Improve weather display with Adafruit's design

--- epaper-weather.py
+++ epaper-weather.py
@@ -1,68 +1,216 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-import epd2in13_V2
-import requests, json
+#!/usr/bin/python3
+#Raspberry Pi Zero e-ink weather display using NOAA API
+#Inspired by https://learn.adafruit.com/raspberry-pi-e-ink-weather-station-using-python
+
+import atexit
+import datetime
+import json
 import time
-from PIL import Image,ImageDraw,ImageFont
-from datetime import datetime
-import traceback
-import threading
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from waveshare_epd.lib.waveshare_epd import epd2in13_V2
 
-REFRESH_INTERVAL = 1 # in minutes
 
-#Prepare display
-epd = epd2in13_V2.EPD()
-print("Init and clear")
-epd.init(epd.FULL_UPDATE)
-epd.Clear(0xff)
+LOCATION_TEXT = "Seattle, WA"
+LOCATION_LATLON = "47.6597,-122.3191"
+LOCATION_URL = "https://api.weather.gov/points/%s" % LOCATION_LATLON
+FORECAST_URL = "https://api.weather.gov/gridpoints/%s/%s,%s/forecast"
 
-font12 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 12)
-font16 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 16)
+FONT_SMALL = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 16)
+FONT_MEDIUM = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20)
+FONT_LARGE = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24)
+FONT_ICON = ImageFont.truetype("./meteocons.ttf", 48)
 
-def draw_weather(locale, temp, next_rain):
-    HBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
-    #HRYimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red or yellow image
-    drawblack = ImageDraw.Draw(HBlackimage)
-    #drawry = ImageDraw.Draw(HRYimage)
+ICON_MAP = {
+    "skc": "BC",              #Fair/clear
+    "few": "BC",              #A few clouds
+    "sct": "HI",              #Partly cloudy
+    "bkn": "NN",              #Mostly cloudy
+    "ovc": "YY",              #Overcast
+    "wind_skc": "FF",         #Fair/clear and windy
+    "wind_few": "FF",         #A few clouds and windy
+    "wind_sct": "SS",         #Partly cloudy and windy
+    "wind_bkn": "SS",         #Mostly cloudy and windy
+    "wind_ovc": "SS",         #Overcast and windy
+    "snow": "VV",             #Snow
+    "rain_snow": "XX",        #Rain/snow
+    "rain_sleet": "XX",       #Rain/sleet
+    "snow_sleet": "WW",       #Snow/sleet
+    "fzra": "RR",             #Freezing rain
+    "rain_fzra": "RR",        #Rain/freezing rain
+    "snow_fzra": "WW",        #Freezing rain/snow
+    "sleet": "XX",            #Sleet
+    "rain": "RR",             #Rain
+    "rain_showers": "QQ",     #Rain showers (high cloud cover)
+    "rain_showers_hi": "QQ",  #Rain showers (low cloud cover)
+    "tsra": "00",             #Thunderstorm (high cloud cover)
+    "tsra_sct": "00",         #Thunderstorm (medium cloud cover)
+    "tsra_hi": "00",          #Thunderstorm (low cloud cover)
+    "tornado": "((",          #Tornado
+    "hurricane": "((",        #Hurricane conditions
+    "tropical_storm": "((",   #Tropical storm conditions
+    "dust": "EE",             #Dust
+    "smoke": "EE",            #Smoke
+    "haze": "JK",             #Haze
+    "hot": "''",              #Hot
+    "cold": "GG",             #Cold
+    "blizzard": "WW",         #Blizzard
+    "fog": "JK",              #Fog/mist
+}
 
-    margin_left = 4
-    margin_top = 20
-    datetime_str = time.strftime("%H:%M on %m/%d", time.localtime())
 
-    drawblack.text((margin_left, margin_top + 0), f"Weather for {locale} @ {datetime_str}", font = font12, fill = 0)
-    drawblack.text((margin_left, margin_top + 16), f"Temp: {temp}", font = font16, fill = 0)
-    if next_rain:
-        drawblack.text((margin_left, margin_top + 36), f"Rain @ {next_rain.hour}:00 on {next_rain.month}/{next_rain.day}", font = font16, fill = 0)
+class WeatherGraphics:
+    def __init__(self, display, locationText):
+        self._display = display
+        self._image = None
+
+        self._weatherIcon = None
+        self._locationText = locationText
+        self._mainText = None
+        self._temperature = None
+        self._description = None
+        self._timeText = None
+
+    def updateWeather(self, forecast):
+        weather = forecast["properties"]["periods"][0]
+
+        iconId = weather["icon"].split('?', 1)[0].rsplit('/', 2)
+        self._weatherIcon = ICON_MAP[iconId[2]][0 if iconId[1] == "day" else 1]
+
+        main = weather["shortForecast"]
+        print(main)
+        self._mainText = main
+
+        temperature = "%d °%s" % (weather["temperature"], weather["temperatureUnit"])
+        print(temperature)
+        self._temperature = temperature
+
+        description = weather["detailedForecast"].split('.', 1)[0] + "."
+        print(description)
+        self._description = description
+
+        self.updateTime()
+
+    def updateTime(self):
+        now = datetime.datetime.now()
+        self._timeText = now.strftime("%I:%M %p").lstrip("0").replace(" 0", " ")
+
+    def updateImage(self):
+        displayWidth = self._display.height
+        displayHeight = self._display.width
+
+        image = Image.new('1', (displayWidth, displayHeight), color=0xff)
+        draw = ImageDraw.Draw(image)
+
+        # Draw the icon
+        (fontWidth, fontHeight) = FONT_ICON.getsize(self._weatherIcon)
+        draw.text(
+            (
+                displayWidth // 2 - fontWidth // 2,
+                displayHeight // 2 - fontHeight // 2 - 5
+            ),
+            self._weatherIcon,
+            font=FONT_ICON,
+            fill=0x00,
+        )
+
+        # Draw the location text
+        draw.text(
+            (5, 5),
+            self._locationText,
+            font=FONT_MEDIUM,
+            fill=0x00,
+        )
+
+        # Draw the time
+        (fontWidth, fontHeight) = FONT_MEDIUM.getsize(self._timeText)
+        draw.text(
+            (5, fontHeight * 2 - 5),
+            self._timeText,
+            font=FONT_MEDIUM,
+            fill=0x00,
+        )
+
+        # Draw the main text
+        (fontWidth, fontHeight) = FONT_LARGE.getsize(self._mainText)
+        draw.text(
+            (5, displayHeight - fontHeight - 5), #(5, displayHeight - fontHeight * 2),
+            self._mainText,
+            font=FONT_LARGE,
+            fill=0x00,
+        )
+
+        # Draw the description text
+        #(fontWidth, fontHeight) = FONT_SMALL.getsize(self._description)
+        #draw.text(
+        #    (5, displayHeight - fontHeight - 5),
+        #    self._description,
+        #    font=FONT_SMALL,
+        #    fill=0x00,
+        #)
+
+        # Draw the temperature
+        (fontWidth, fontHeight) = FONT_LARGE.getsize(self._temperature)
+        draw.text(
+            (
+                displayWidth - fontWidth - 5,
+                5 #displayHeight - fontHeight * 2
+            ),
+            self._temperature,
+            font=FONT_LARGE,
+            fill=0x00,
+        )
+
+        self._image = image
+
+    def display(self):
+        self.updateImage()
+
+        self._display.init(display.FULL_UPDATE)
+        self._display.Clear(0xff)
+        self._display.display(self._display.getbuffer(self._image))
+        self._display.sleep()
+
+
+#Initialize display
+display = epd2in13_V2.EPD()
+#TODO: Can't reinit after sleep due to bug in Waveshare driver, fix it...
+
+def clearDisplay():
+    display.init(display.FULL_UPDATE)
+    display.Clear(0xff)
+    display.sleep()
+
+atexit.register(clearDisplay)
+
+
+#Query gridpoint
+gridpointData = requests.get(LOCATION_URL)
+if gridpointData.status_code != 200:
+    print("Gridpoint query failed: %d", gridpointData.status_code)
+    exit(1)
+
+gridpoint = gridpointData.json()
+
+
+#Display weather
+displayGFX = WeatherGraphics(display, LOCATION_TEXT)
+displayRefresh = 0
+
+while True:
+    #Update once every 15 minutes (on the quarter hour)
+    if time.monotonic() - displayRefresh < 60 or (datetime.datetime.now().minute % 15 != 0 and displayRefresh != 0):
+        time.sleep(30)
+        continue
+
+    #Query weather and update display
+    forecastUrl = FORECAST_URL % (gridpoint["properties"]["gridId"], gridpoint["properties"]["gridX"], gridpoint["properties"]["gridY"])
+    forecast = requests.get(forecastUrl)
+    if forecast.status_code == 200:
+        displayGFX.updateWeather(forecast.json())
+        displayGFX.display()
+        displayRefresh = time.monotonic()
     else:
-        drawblack.text((4, margin_top + 36), f"No rain predicted for 48 hrs", font = font16, fill = 0)
+        print("Forecast query failed: %d" % forecast.status_code)
 
-    print("Updating display")
-    epd.display(epd.getbuffer(HBlackimage))#, epd.getbuffer(HRYimage))
-
-def fetch_weather(lat="47.6608",lon="-122.319"):
-
-    pointsUrl = "https://api.weather.gov/points/%s,%s" % (lat, lon)
-    points = requests.get(pointsUrl).json()
-
-    forecastUrl = "https://api.weather.gov/gridpoints/%s/%s,%s/forecast" % (points["properties"]["gridId"], points["properties"]["gridX"], points["properties"]["gridY"])
-    forecast = requests.get(forecastUrl).json()
-    temperature = forecast["properties"]["periods"][0]["temperature"]
-
-    print("Weather: %s, %sF" % (points["properties"]["gridId"], temperature))
-
-    ##threading.Timer(60.0 * REFRESH_INTERVAL, fetch_weather).start()
-    #base_url = "http://api.openweathermap.org/data/2.5/onecall"
-    #req_url = f"{base_url}?appid={api_key}&exclude=minutely&lat=40.746&lon=-73.978&units=imperial"
-    #response = requests.get(req_url) 
-    #raw_data = response.json()
-    #temp_F = raw_data["current"]["temp"]
-    #rounded_temp = round(temp_F, 0)
-    #hourly_data = raw_data["hourly"]
-    #hourly_will_rain = [x["dt"] for x in hourly_data if "rain" in x["weather"][0]["main"].lower()]
-    #next_rain = datetime.fromtimestamp(min(hourly_will_rain)) if len(hourly_will_rain) > 0 else None
-    draw_weather(points["properties"]["gridId"], temperature, None)
-
-fetch_weather()
-epd.Clear(0xff)
-epd2in13_V2.epdconfig.module_exit()
-exit()
+    time.sleep(30)
